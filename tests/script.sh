1743685434993a91a7fece4d7d996ae2b1541a3f
Rodrigo Girorme <rodrigogirorme@gmail.com>
2019-04-23 15:34:23 -0300

    Fixing travis steps script

--- tests/script.sh
+++ tests/script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 php -S 127.0.0.1:8000 -t tests/CurlRox/ &> /dev/null &
 pid="${!}"
-phpunit --configuration ../phpunit.xml --verbose --color=always
+../vendor/bin/phpunit --configuration "./../phpunit.xml" --testdox ../tests --verbose --color=always
